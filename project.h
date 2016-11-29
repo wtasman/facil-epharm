16fe2e95ac28aaa6eb133042acc6d7f61b1c0a9c
Yangyang Herrera <yherrera@e1z3r3p21.42.us.org>
2016-11-29 14:11:38 -0800

    struct added need opinion

--- project.h
+++ project.h
@@ -4,4 +4,27 @@
 # include "libft/libft.h"
 # include "get_next_line.h"
 
+// Pending prototype ideas //
+
+typedef struct		s_drug
+{
+	char			*name;
+	char			*ins;
+	char			**drugi;	//drug interaction
+	char			**diseasei;	//disease interation
+	char			**se;		//side effects
+	struct s_drug	*next;
+}					t_drug;
+
+typedef struct		s_ptnt		//patient
+{
+	char			*name;
+	char			**pres;		//prescription
+	char			**cur_med;	//current medication
+	char			**disease;	//disease and complication
+	struct s_ptnt	*next;
+}					t_ptnt;
+
+// please input your opinion //
+
 #endif
