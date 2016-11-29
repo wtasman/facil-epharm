51e48b8f56af2af5cac9c3ff3dfc2393e641e42a
Matthew Friedrichs <mfriedri@e1z3r3p23.42.us.org>
2016-11-29 15:04:51 -0800

    added color defines for text output

--- project.h
+++ project.h
@@ -3,6 +3,18 @@
 
 # include "libft/libft.h"
 # include "get_next_line.h"
+#include <stdio.h>
+
+#define WARNING     "\x1b[31m"
+#define OK   "\x1b[32m"
+#define INSTRUCTIONS  "\x1b[33m"
+/*
+printf(INSTRUCTIONS     "TAKE BEFORE BED WITH DRINK"     ANSI_COLOR_RESET "\n");
+*/
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
 typedef struct		s_drug
 {
