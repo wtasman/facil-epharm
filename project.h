d65d5c6048e3ccf91d858d7a6a32c1681c189b1c
Yangyang Herrera <yherrera@e1z3r3p21.42.us.org>
2016-11-29 19:20:35 -0800

    side effect cmp function finished, further testing needed

--- project.h
+++ project.h
@@ -5,16 +5,16 @@
 # include "get_next_line.h"
 #include <stdio.h>
 
-#define WARNING     "\x1b[31m"
-#define OK   "\x1b[32m"
-#define INSTRUCTIONS  "\x1b[33m"
-/*
-printf(INSTRUCTIONS     "TAKE BEFORE BED WITH DRINK"     ANSI_COLOR_RESET "\n");
-*/
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+// #define WARNING     "\x1b[31m"
+// #define OK   "\x1b[32m"
+// #define INSTRUCTIONS  "\x1b[33m"
+
+// printf(INSTRUCTIONS     "TAKE BEFORE BED WITH DRINK"     ANSI_COLOR_RESET "\n");
+
+// #define ANSI_COLOR_BLUE    "\x1b[34m"
+// #define ANSI_COLOR_MAGENTA "\x1b[35m"
+// #define ANSI_COLOR_CYAN    "\x1b[36m"
+// #define ANSI_COLOR_RESET   "\x1b[0m"
 
 typedef struct		s_drug
 {
@@ -33,5 +33,8 @@ typedef struct		s_ptnt
 	char			**cur_med;
 	char			**disease;
 }					t_ptnt;
+
+int					ft_strcmp(const char *s1, const char *s2);
+void				dis_se_cmp(t_drug *pres, t_ptnt patient);
 
 #endif
