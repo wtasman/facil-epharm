abfe003959cd7fa64041343f508c20b99b268056
William Asman <wasman@e1z3r3p1.42.us.org>
2016-11-29 19:01:02 -0800

    adding a rmv_title function & updated the header

---
+++ rmv_title.c
@@ -0,0 +1,17 @@
+char	*rmv_title(char *str)
+{
+	int i;
+	char *tmp;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '-')
+		{
+			tmp = ft_strsub(str, (i + 2), (ft_strlen(str) - (i + 1)));
+			return (tmp);
+		}
+		i++;
+	}
+	return (str);
+}
