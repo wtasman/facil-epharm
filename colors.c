e5eb3ae4446f3f06293907bcffc9b95acc2ed96b
Yangyang Herrera <yherrera@e1z3r3p21.42.us.org>
2016-11-30 14:35:43 -0800

    color fixed

--- colors.c
+++ colors.c
@@ -2,30 +2,30 @@
 
 void	drug_name(char *drug)
 {
-	printf(DRUG_NAME "Prescribed Drug: %s\n" ANSI_COLOR_RESET, drug);
+	printf(DRUG_NAME "Prescribed Drug: %s" ANSI_COLOR_RESET "\n", drug);
 }
 
 void	drug_conflict(char *conflict)
 {
-	printf(WARNING "WARNING: conflict with %s\n" ANSI_COLOR_RESET, conflict);
+	printf(WARNING "WARNING: conflict with %s" ANSI_COLOR_RESET "\n", conflict);
 }
 
 void	side_conflict(char *conflict)
 {
-	printf(WARNING "WARNING: patent already has %s\n" ANSI_COLOR_RESET, conflict);
+	printf(WARNING "WARNING: patent already has %s" ANSI_COLOR_RESET "\n", conflict);
 }
 
 void	instructions(char *instructions)
 {
-	printf(INSTRUCTIONS "%s\n" ANSI_COLOR_RESET, instructions);
+	printf(INSTRUCTIONS "%s" ANSI_COLOR_RESET "\n", instructions);
 }
 
 void	drug_clear(void)
 {
-	printf(OK "No drug conflicts!\n" ANSI_COLOR_RESET);
+	printf(OK "No drug conflicts!" ANSI_COLOR_RESET "\n");
 }
 
 void	side_clear(void)
 {
-	printf(OK "No side effect conflicts!\n" ANSI_COLOR_RESET);
+	printf(OK "No side effect conflicts!" ANSI_COLOR_RESET "\n");
 }
